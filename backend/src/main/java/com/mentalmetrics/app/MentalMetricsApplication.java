00ebf12df22c54a2f46513fd0c3d19aa4469ac03
Jackson Wozniak <jjw30@comcast.net>
2025-08-13 22:03:10 -0400

    initial commit of frontend and backend apps

---
+++ backend/src/main/java/com/mentalmetrics/app/MentalMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.mentalmetrics.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalMetricsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/mentalmetrics/app/MentalMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentalmetrics.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
